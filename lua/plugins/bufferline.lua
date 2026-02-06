1d07552d0e180334b191539815daef1b3cae47a1
Jack McGinty <jtmcginty16@gmail.com>
2026-02-06 12:14:11 -0600

    feat(bufferline): add visual buffer tabs with navigation keymaps
    - Install bufferline.nvim for visual buffer tabs at top
    - Configure with mouse support and LSP diagnostics
    - Add ]b/[b for cycling through buffers
    - Add <leader>bd to close current buffer
    - Add <leader>bo to close other buffers
    - Integrate with neo-tree offset

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,57 @@
+-- ============================================================================
+-- Bufferline: Visual Buffer Tabs
+-- ============================================================================
+-- Shows open buffers as tabs at the top of the screen
+
+return {
+  'akinsho/bufferline.nvim',
+  version = '*',
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  event = 'VeryLazy',
+  config = function()
+    require('bufferline').setup({
+      options = {
+        mode = 'buffers',
+        themable = true,
+        numbers = 'none',
+        close_command = 'bdelete! %d',
+        right_mouse_command = 'bdelete! %d',
+        left_mouse_command = 'buffer %d',
+        middle_mouse_command = nil,
+        indicator = {
+          style = 'underline',
+        },
+        buffer_close_icon = '󰅖',
+        modified_icon = '●',
+        close_icon = '',
+        left_trunc_marker = '',
+        right_trunc_marker = '',
+        diagnostics = 'nvim_lsp',
+        diagnostics_indicator = function(count, level)
+          local icon = level:match('error') and ' ' or ' '
+          return ' ' .. icon .. count
+        end,
+        offsets = {
+          {
+            filetype = 'neo-tree',
+            text = 'File Explorer',
+            text_align = 'center',
+            separator = true,
+          },
+        },
+        show_buffer_icons = true,
+        show_buffer_close_icons = true,
+        show_close_icon = false,
+        show_tab_indicators = true,
+        separator_style = 'thin',
+        always_show_bufferline = true,
+      },
+    })
+
+    -- Keymaps for buffer navigation
+    vim.keymap.set('n', ']b', '<cmd>BufferLineCycleNext<CR>', { desc = 'Next buffer' })
+    vim.keymap.set('n', '[b', '<cmd>BufferLineCyclePrev<CR>', { desc = 'Previous buffer' })
+    vim.keymap.set('n', '<leader>bd', '<cmd>bdelete<CR>', { desc = 'Close buffer' })
+    vim.keymap.set('n', '<leader>bo', '<cmd>BufferLineCloseOthers<CR>', { desc = 'Close other buffers' })
+  end,
+}
