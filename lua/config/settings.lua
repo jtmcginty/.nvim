44f80ec817407f40f8da105ec7061d79b569b4f6
Jack McGinty <jtmcginty16@gmail.com>
2026-02-06 15:21:05 -0600

    fix(keymaps): move quality-of-life keymaps to settings.lua
    - Move keymaps from plugin file to settings.lua for proper loading
    - Fake plugin entry wasn't loading keymaps correctly
    - All keymaps now load on startup

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -52,6 +52,37 @@ vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move to upper window' })
 -- Editor Settings
 -- ============================================================================
 
+-- Clear search highlights on <Esc>
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear search highlights' })
+
+-- Auto-center screen after jumps
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Jump down half page (centered)' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Jump up half page (centered)' })
+vim.keymap.set('n', 'G', 'Gzz', { desc = 'Jump to end of file (centered)' })
+
+-- Move selected lines up/down in visual mode
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
+
+-- Keep cursor in place when joining lines
+vim.keymap.set('n', 'J', 'mzJ`z', { desc = 'Join lines (keep cursor position)' })
+
+-- Disable Q (prevents accidental macro replay)
+vim.keymap.set('n', 'Q', '<nop>', { desc = 'Disabled' })
+
+-- Easier terminal exit
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- Toggle line wrap
+vim.keymap.set('n', '<leader>tw', function()
+  vim.opt.wrap = not vim.opt.wrap:get()
+  print('Line wrap: ' .. (vim.opt.wrap:get() and 'ON' or 'OFF'))
+end, { desc = 'Toggle line wrap' })
+
+-- ============================================================================
+-- Editor Settings
+-- ============================================================================
+
 -- Line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
