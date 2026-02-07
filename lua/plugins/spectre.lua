b0c6179ed5c9af1a14a0a712ddd68e4a83125976
Jack McGinty <jtmcginty16@gmail.com>
2026-02-07 17:37:53 -0600

    feat: add Spectre for project-wide search and replace
    - Add <leader>sr to open Spectre search/replace panel
    - Add <leader>sw to search current word
    - Add <leader>sp to search in current file only
    - Visual preview of all changes before applying
    - Safe refactoring across entire project

---
+++ lua/plugins/spectre.lua
@@ -0,0 +1,30 @@
+-- ============================================================================
+-- Spectre: Project-wide Search and Replace
+-- ============================================================================
+-- Visual search and replace with preview
+
+return {
+  'nvim-pack/nvim-spectre',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  keys = {
+    { '<leader>sr', '<cmd>lua require("spectre").toggle()<cr>', desc = 'Search and Replace (Spectre)' },
+    { '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<cr>', desc = 'Search current Word (Spectre)' },
+    { '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<cr>', desc = 'Search in current file (Spectre)' },
+  },
+  config = function()
+    require('spectre').setup({
+      open_cmd = 'vnew',
+      live_update = false, -- Auto-execute search on typing (can be slow)
+      line_sep_start = '┌-----------------------------------------',
+      result_padding = '¦  ',
+      line_sep       = '└-----------------------------------------',
+      highlight = {
+        ui = 'String',
+        search = 'DiffChange',
+        replace = 'DiffDelete',
+      },
+    })
+  end,
+}
