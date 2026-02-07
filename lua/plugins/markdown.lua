a38be68cef637df6d4e2aeedf5a8048060646339
Jack McGinty <jtmcginty16@gmail.com>
2026-02-06 20:35:28 -0600

    feat: enable external access for markdown preview
    - Allow connections from remote clients
    - Echo preview URL to messages

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -11,7 +11,9 @@ return {
       { "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview" },
     },
     config = function()
-      vim.g.mkdp_port = '8765' -- Fixed port for SSH tunneling
+      vim.g.mkdp_port = '8765' -- Fixed port for consistent preview server
+      vim.g.mkdp_open_to_the_world = 1 -- Allow external connections
+      vim.g.mkdp_echo_preview_url = 1 -- Print URL to messages
     end,
   },
 }
